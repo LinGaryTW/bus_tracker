9b91522f6aa30d3b4ebb8119795f8982b0941253
Gary Lin <garyqwertyuiop@gmail.com>
2022-05-27 00:16:30 +0800

    add time gap when bus driving

--- demo_script.rb
+++ demo_script.rb
@@ -8,34 +8,46 @@ gary = User.new('Gary')
 alan = User.new('Alan')
 scott = User.new('Scott')
 
-p '目前有兩位旅客Gary, Alan 想在博仁醫院上車'
-sleep 1
+puts '目前有兩位旅客Gary, Alan 想在博仁醫院上車 他們應該在公車抵達市民住宅時會收到通知'
+sleep 2
 
 gary.set_alert_for_target_station(line672_to_Dapeng, '博仁醫院')
 alan.set_alert_for_target_station(line672_to_Dapeng, '博仁醫院')
 
-p 'Scott想要在三民路上車'
-sleep 1
+puts 'Scott想要在三民路上車 他應該會在公車抵達國父紀念館時收到通知'
+sleep 2
 
 scott.set_alert_for_target_station(line672_to_Dapeng, '三民路')
 
-p "672 往 大鵬新城 即將發車"
+puts "672 往 大鵬新城 發車囉！"
 bus = Bus.new
 line672_to_Dapeng.new_bus_start_serving_from(0, bus)
-
 bus.leave
+
+puts '.'
+sleep 1
+puts '.'
+sleep 1
+puts '.'
 sleep 1
 
-p "672 抵達市民住宅"
+puts "672 抵達市民住宅"
 
 bus.arrive
 sleep 1
 
-p "672 即將前往國父紀念館"
+puts "672 即將前往國父紀念館"
 
 bus.leave
+
+sleep 1
+puts '.'
+sleep 1
+puts '.'
+sleep 1
+puts '.'
 sleep 1
 
-p "672 抵達國父紀念館"
+puts "672 抵達國父紀念館"
 
 bus.arrive
